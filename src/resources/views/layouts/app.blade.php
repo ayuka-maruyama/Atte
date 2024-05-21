fca07f6d69cd4b15b0357eef131f961dc07b0053
ayuka <basketball110512@gmail.com>
2024-05-21 20:18:55 +0900

    update app.blade.php css読み込み追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- cssファイル作成後に記述（共通分） -->
+    <link rel="stylesheet" href="{{ asset('css/destyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
     <title>Document</title>
 </head>
