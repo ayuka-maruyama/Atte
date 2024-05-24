0b653b78afb34a86646a6820d8eb5e9e995d3e8c
ayuka <basketball110512@gmail.com>
2024-05-24 17:07:21 +0900

    update 打刻ページview&css、共通CSS、コントローラ

--- src/app/Http/Controllers/RegisteredUserController.php
+++ src/app/Http/Controllers/RegisteredUserController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class RegisteredUserController extends Controller
 {
     public function create() {
-        return view('auth/register');
+        return view('stamp');
     }
 }
