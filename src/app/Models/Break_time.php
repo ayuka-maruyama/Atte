768e3563657cb5345eecb3df407d70cffbd7e7c2
ayuka <basketball110512@gmail.com>
2024-06-11 02:46:28 +0900

    Break_timeモデルの更新

--- src/app/Models/Break_time.php
+++ src/app/Models/Break_time.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Break_time extends Model
 {
     use HasFactory;
+
+    protected $table = 'break_times';
+    protected $fillable = ['work_time_id', 'break_start_time', 'break_end_time'];
+    protected $dates = ['created_at', 'updated_at'];
+
+    /**
+     * User関連付け
+     * 1対多
+     */
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
 }
