51d382d5c0355aa35da057cfddd8084a0663c571
ayuka <basketball110512@gmail.com>
2024-06-17 05:26:07 +0900

    日付別勤怠情報一覧ページのコントローラ・ビュー修正

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -2,12 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Work_time;
+use App\Models\Break_time;
 use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    public function attendance()
+    public function attendance(Request $request)
     {
-        return view('date');
+        $date = $request->input('date', Carbon::today()->format("Y-m-d"));
+
+        $workRecords = Work_time::where('date', $date)
+            ->with('user')
+            ->paginate(5);
+
+        $workId = $workRecords->pluck('id');
+
+        $breakRecords = Break_time::whereIn('work_time_id', $workId)
+            ->with('work_time')
+            ->get();
+
+        $breakTimesByWorkId = [];
+
+        foreach ($breakRecords as $breakRecord) {
+            $workTimeId = $breakRecord->work_time_id;
+
+            if (!isset($breakTimesByWorkId[$workTimeId])) {
+                $breakTimesByWorkId[$workTimeId] = 0;
+            }
+
+            $breakStartTime = strtotime($breakRecord->break_start_time);
+            $breakEndTime = strtotime($breakRecord->break_end_time);
+            $breakSecond = $breakEndTime - $breakStartTime;
+            $breakTimesByWorkId[$workTimeId] += $breakSecond;
+        }
+
+        $pureWorkTimes = [];
+
+        foreach ($workRecords as $workRecord) {
+            $workStartTime = strtotime($workRecord->start_time);
+
+            if ($workRecord->end_time) {
+                $workEndTime = strtotime($workRecord->end_time);
+                $workDuration = $workEndTime - $workStartTime;
+
+                $breakDuration = isset($breakTimesByWorkId[$workRecord->id]) ? $breakTimesByWorkId[$workRecord->id] : 0;
+
+                $actualWorkTimeInSeconds = $workDuration - $breakDuration;
+
+                $pureWorkTimes[$workRecord->id] = gmdate("H:i:s", $actualWorkTimeInSeconds);
+            } else {
+                $pureWorkTimes[$workRecord->id] = '勤務中';
+            }
+        }
+
+        foreach ($breakTimesByWorkId as $workTimeId => $breakTimeInSeconds) {
+            $breakTimesByWorkId[$workTimeId] = gmdate("H:i:s", $breakTimeInSeconds);
+        }
+
+        return view('date', compact('date', 'workRecords', 'breakTimesByWorkId', 'pureWorkTimes'));
     }
 }
--- src/resources/views/date.blade.php
+++ src/resources/views/date.blade.php
@@ -1,83 +1,56 @@
 @extends('layouts.app')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/date.css') }}">
+<link rel="stylesheet" href="{{ asset('css/date.css') }}">
 @endsection
 
 @section('header')
-    <nav class="header__nav">
-        <ul class="header__nav-ul">
-            <li class="header__nav-item">
-                <a href="/" class="header__link">ホーム</a>
-            </li>
-            <li class="header__nav-item">
-                <a href="/attendance" class="header__link">日付一覧</a>    
-            </li>
-            <li class="header__nav-item">
-                <form class="header__logout" action="/logout" method="post">
+<nav class="header__nav">
+    <ul class="header__nav-ul">
+        <li class="header__nav-item">
+            <a href="/" class="header__link">ホーム</a>
+        </li>
+        <li class="header__nav-item">
+            <a href="/attendance" class="header__link">日付一覧</a>
+        </li>
+        <li class="header__nav-item">
+            <form class="header__logout" action="/logout" method="post">
                 @csrf
-                    <button class="header__nav-button" type="submit">ログアウト</button>
-                </form>
-            </li>
-        </ul>
-    </nav>
+                <button class="header__nav-button" type="submit">ログアウト</button>
+            </form>
+        </li>
+    </ul>
+</nav>
 @endsection
 
 @section('content')
+<div class="attendance">
     <div class="date">
-        <h3 class="date__title">2024-05-25</h3>
-        <!-- テーブル作成後日付を取得して表示させる -->
-        <div class="work-time">
-            <table class="work-time__table">
-                <tr class="table__row">
-                    <th class="work-time__title">名前</th>
-                    <th class="work-time__title">勤務開始</th>
-                    <th class="work-time__title">勤務終了</th>
-                    <th class="work-time__title">休憩時間</th>
-                    <th class="work-time__title">勤務時間</th>
-                </tr>
-                <tr class="table__row">
-                    <!-- テーブル作成後、それぞれの項目を表示させる -->
-                    <td class="work-time__item">テスト太郎</td>
-                    <td class="work-time__item">10:00:00</td>
-                    <td class="work-time__item">20:00:00</td>
-                    <td class="work-time__item">00:30:00</td>
-                    <td class="work-time__item">09:30:00</td>
-                </tr>
-                <tr class="table__row">
-                    <!-- テーブル作成後、それぞれの項目を表示させる -->
-                    <td class="work-time__item">テスト太郎</td>
-                    <td class="work-time__item">10:00:00</td>
-                    <td class="work-time__item">20:00:00</td>
-                    <td class="work-time__item">00:30:00</td>
-                    <td class="work-time__item">09:30:00</td>
-                </tr>
-                <tr class="table__row">
-                    <!-- テーブル作成後、それぞれの項目を表示させる -->
-                    <td class="work-time__item">テスト太郎</td>
-                    <td class="work-time__item">10:00:00</td>
-                    <td class="work-time__item">20:00:00</td>
-                    <td class="work-time__item">00:30:00</td>
-                    <td class="work-time__item">09:30:00</td>
-                </tr>
-                <tr class="table__row">
-                    <!-- テーブル作成後、それぞれの項目を表示させる -->
-                    <td class="work-time__item">テスト太郎</td>
-                    <td class="work-time__item">10:00:00</td>
-                    <td class="work-time__item">20:00:00</td>
-                    <td class="work-time__item">00:30:00</td>
-                    <td class="work-time__item">09:30:00</td>
-                </tr>
-                <tr class="table__row">
-                    <!-- テーブル作成後、それぞれの項目を表示させる -->
-                    <td class="work-time__item">テスト太郎</td>
-                    <td class="work-time__item">10:00:00</td>
-                    <td class="work-time__item">20:00:00</td>
-                    <td class="work-time__item">00:30:00</td>
-                    <td class="work-time__item">09:30:00</td>
-                </tr>
-            </table>
-            <!-- ページネーションの部分を記述する -->
-        </div>
+        <!-- 前日、翌日の動作はまだ記述していない -->
+        <a href="" class="date__title-before">&lt;</a>
+        <h3 class="date__title">{{ $date }}</h3>
+        <a href="" class="date__title-after">&gt;</a>
     </div>
+    <div class="work-time">
+        <table class="work-time__table">
+            <tr class="table__row">
+                <th class="work-time__title">名前</th>
+                <th class="work-time__title">勤務開始</th>
+                <th class="work-time__title">勤務終了</th>
+                <th class="work-time__title">休憩時間</th>
+                <th class="work-time__title">勤務時間</th>
+            </tr>
+            @foreach($workRecords as $record)
+            <tr class="table__row">
+                <td class="work-time__item">{{ $record->user->name }}</td>
+                <td class="work-time__item">{{ $record->start_time }}</td>
+                <td class="work-time__item">{{ $record->end_time }}</td>
+                <td class="work-time__item">{{ $breakTimesByWorkId[$record->id] ?? '00:00:00' }}</td>
+                <td class="work-time__item">{{ $pureWorkTimes[$record->id] ?? '00:00:00' }}</td>
+            </tr>
+            @endforeach
+        </table>
+        {{ $workRecords->links() }}
+    </div>
+</div>
 @endsection
